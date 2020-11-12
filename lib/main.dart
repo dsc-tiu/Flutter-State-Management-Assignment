4908c3a59fd0ed5efab5d65c077eb8004445dc1a
AnkanPaul7 <54813730+AnkanPaul7@users.noreply.github.com>
2020-11-12 20:41:32 +0530

    Update - main.dart [included state management features]
    Added Floating Action Buttons for Addition, Subtraction, Division and Multiplication using Stream()

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -28,29 +29,32 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  StreamController _streamController;
+  Stream _stream;
+
+  @override
+  void initState() {
+    _streamController = StreamController();
+    _stream = _streamController.stream;
+  }
 
   void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+    _streamController.sink.add(_counter++);
   }
 
   void _decrementCounter() {
-    setState(() {
-      _counter--;
-    });
+    _streamController.sink.add(_counter--);
   }
 
   void _multiplyCounter() {
-    setState(() {
-      _counter *= 2;
-    });
+    _counter = _counter * 2;
+    _streamController.sink.add(_counter);
   }
+
   void _divideCounter() {
-    setState(() {
-      double x=_counter/2;
-      _counter=x.toInt();
-    });
+    double d = _counter / 2;
+    _counter = d.toInt();
+    _streamController.sink.add(_counter);
   }
 
   @override
@@ -66,47 +70,47 @@ class _MyHomePageState extends State<MyHomePage> {
             Text(
               'You have pushed the button this many times:',
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
+            StreamBuilder(
+                stream: _stream,
+                builder: (BuildContext context, AsyncSnapshot snapshot) {
+                  return Text(
+                    snapshot.data != null ? snapshot.data.toString() : "0",
+                    style: Theme.of(context).textTheme.display1,
+                  );
+                })
           ],
         ),
       ),
-      floatingActionButton: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: <Widget>[
-          FloatingActionButton(
-            onPressed: _incrementCounter,
-            tooltip: 'Increment',
-            child: Icon(Icons.add),
-          ),
-          SizedBox(
-            height: 16,
-          ),
-          FloatingActionButton(
-            onPressed: _decrementCounter,
-            // tooltip: 'Decrement',
-            child: Icon(Icons.remove),
-          ),
-          SizedBox(
-            height: 16,
-          ),
-          FloatingActionButton(
-            onPressed: _multiplyCounter,
-            tooltip: 'Multiply',
-            child: Icon(Icons.clear),
-          ),
-          SizedBox(
-            height: 16,
-          ),
-          FloatingActionButton(
-            onPressed: _divideCounter,
-            tooltip: 'Divide',
-            child: Icon(Icons.create),
-          ),
-        ],
-      ),
+      floatingActionButton:
+          Column(mainAxisAlignment: MainAxisAlignment.end, children: <Widget>[
+        FloatingActionButton(
+          onPressed: _incrementCounter,
+          tooltip: 'Increment',
+          child: Icon(Icons.add),
+        ),
+        SizedBox(height: 20),
+        FloatingActionButton(
+          onPressed: _decrementCounter,
+          tooltip: 'Decrement',
+          child: Icon(Icons.remove),
+        ),
+            SizedBox(height: 20),
+        FloatingActionButton(
+          onPressed: _multiplyCounter,
+          tooltip: 'Multiply',
+          child: Icon(Icons.clear),
+        ),
+            SizedBox(height: 20),
+        FloatingActionButton(
+          onPressed: _divideCounter,
+          tooltip: 'Divide',
+          child: Icon(Icons.edit),
+        ),
+      ] // This trailing comma makes auto-formatting nicer for build methods.
+              ),
     );
   }
+
+  @override
+  void dispose() {}
 }
